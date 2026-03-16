dee6c779ea589c9ca8fe122ae8553df1282c11ff
sahaarnav3 <sahaarnav3@gmail.com>
2026-03-16 13:38:01 +0530

    Exercise 1.6 Done

---
+++ part_1/unicafe/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+function App() {
+  const [goodCount, setGoodCount] = useState(0);
+  const [neutralCount, setNeutralCount] = useState(0);
+  const [badCount, setBadCount] = useState(0);
+
+  return (
+    <>
+      <h1>give feedback</h1>
+      <div>
+        <button onClick={() => setGoodCount(goodCount + 1)}>good</button>
+        <button onClick={() => setNeutralCount(neutralCount + 1)}>neutral</button>
+        <button onClick={() => setBadCount(badCount + 1)}>bad</button>
+      </div>
+      <div>
+        <h1>statistics</h1>
+        <div>
+          <p>good {goodCount}</p>
+          <p>neutral {neutralCount}</p>
+          <p>bad {badCount}</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
