3ae3ffe9b97a007467ac84e1f5a620bc53948171
sahaarnav3 <sahaarnav3@gmail.com>
2026-03-16 18:26:51 +0530

    Exercise 1.10 Done

--- part_1/unicafe/src/App.jsx
+++ part_1/unicafe/src/App.jsx
@@ -9,11 +9,9 @@ function App() {
     <>
       <h1>give feedback</h1>
       <div>
-        <button onClick={() => setGoodCount(goodCount + 1)}>good</button>
-        <button onClick={() => setNeutralCount(neutralCount + 1)}>
-          neutral
-        </button>
-        <button onClick={() => setBadCount(badCount + 1)}>bad</button>
+        <Button text="good" valueSetter={() => setGoodCount(goodCount + 1)} />
+        <Button text="neutral" valueSetter={() => setNeutralCount(neutralCount + 1)} />
+        <Button text="bad" valueSetter={() => setBadCount(badCount + 1)} />
       </div>
       <h1>statistics</h1>
       {goodCount || neutralCount || badCount ? (
@@ -32,18 +30,26 @@ function App() {
 const Statistics = ({ goodCount, neutralCount, badCount }) => {
   return (
     <div>
-      <p>good {goodCount}</p>
-      <p>neutral {neutralCount}</p>
-      <p>bad {badCount}</p>
-      <p>all {goodCount + badCount + neutralCount}</p>
-      <p>
-        average {(goodCount - badCount) / (goodCount + badCount + neutralCount)}
-      </p>
-      <p>
-        positive {(goodCount / (goodCount + badCount + neutralCount)) * 100}%
-      </p>
+      <StatisticLine text="good" value={goodCount} />
+      <StatisticLine text="neutral" value={neutralCount} />
+      <StatisticLine text="bad" value={badCount} />
+      <StatisticLine text="all" value={goodCount + badCount + neutralCount} />
+      <StatisticLine text="average" value={(goodCount - badCount) / (goodCount + badCount + neutralCount)} />
+      <StatisticLine text="positive" value={(goodCount / (goodCount + badCount + neutralCount)) * 100} />
     </div>
   );
 };
+
+const StatisticLine = ({text, value}) => {
+  return (
+    <p>{text} {value} {text === "positive" ? "%" : ""}</p>
+  )
+}
+
+const Button =({text, valueSetter}) => {
+  return (
+    <button onClick={valueSetter}>{text}</button>
+  )
+}
 
 export default App;
