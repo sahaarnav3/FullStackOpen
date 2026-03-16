e63a91b3e9b948fcb2a7a8ff3856a26e06d81729
sahaarnav3 <sahaarnav3@gmail.com>
2026-03-16 18:47:24 +0530

    Exercise 1.11 Done

--- part_1/unicafe/src/App.jsx
+++ part_1/unicafe/src/App.jsx
@@ -10,7 +10,10 @@ function App() {
       <h1>give feedback</h1>
       <div>
         <Button text="good" valueSetter={() => setGoodCount(goodCount + 1)} />
-        <Button text="neutral" valueSetter={() => setNeutralCount(neutralCount + 1)} />
+        <Button
+          text="neutral"
+          valueSetter={() => setNeutralCount(neutralCount + 1)}
+        />
         <Button text="bad" valueSetter={() => setBadCount(badCount + 1)} />
       </div>
       <h1>statistics</h1>
@@ -29,27 +32,38 @@ function App() {
 
 const Statistics = ({ goodCount, neutralCount, badCount }) => {
   return (
-    <div>
+    <table>
       <StatisticLine text="good" value={goodCount} />
       <StatisticLine text="neutral" value={neutralCount} />
       <StatisticLine text="bad" value={badCount} />
       <StatisticLine text="all" value={goodCount + badCount + neutralCount} />
-      <StatisticLine text="average" value={(goodCount - badCount) / (goodCount + badCount + neutralCount)} />
-      <StatisticLine text="positive" value={(goodCount / (goodCount + badCount + neutralCount)) * 100} />
-    </div>
+      <StatisticLine
+        text="average"
+        value={(goodCount - badCount) / (goodCount + badCount + neutralCount)}
+      />
+      <StatisticLine
+        text="positive"
+        value={(goodCount / (goodCount + badCount + neutralCount)) * 100}
+      />
+    </table>
   );
 };
 
-const StatisticLine = ({text, value}) => {
+const StatisticLine = ({ text, value }) => {
   return (
-    <p>{text} {value} {text === "positive" ? "%" : ""}</p>
-  )
-}
+    <tbody>
+      <tr>
+        <td>{text}</td>
+        <td>
+          {value} {text === "positive" ? "%" : ""}
+        </td>
+      </tr>
+    </tbody>
+  );
+};
 
-const Button =({text, valueSetter}) => {
-  return (
-    <button onClick={valueSetter}>{text}</button>
-  )
-}
+const Button = ({ text, valueSetter }) => {
+  return <button onClick={valueSetter}>{text}</button>;
+};
 
 export default App;
