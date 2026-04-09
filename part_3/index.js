3df0db0f25147f2485d3c2fc5713b81ccbee0f92
sahaarnav3 <sahaarnav3@gmail.com>
2026-04-09 21:15:35 +0530

    Modified code after correcting delete service

--- part_3/index.js
+++ part_3/index.js
@@ -57,7 +57,7 @@ app.get("/api/persons/:id", (req, res) => {
 app.delete("/api/persons/:id", (req, res) => {
   const id = req.params.id;
   persons = persons.filter((person) => person.id !== id);
-  res.status(204).end();
+  return res.status(204).end();
 });
 
 const generateId = () => {
