066a8598cc4f23831a0095c8f1a299c1c7397681
sahaarnav3 <sahaarnav3@gmail.com>
2026-04-16 18:35:18 +0530

    Exercise 3.12

---
+++ part_3/mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const password = process.argv[2];
+const userName = process.argv[3];
+const userPhone = process.argv[4];
+
+// console.log(password, userName, userPhone);
+
+const url = `mongodb+srv://neoGStudent:${password}@neog.fc7jd.mongodb.net/fullstackopen?retryWrites=true&w=majority&appName=neoG`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url, { family: 4 });
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (password && !userName && !userPhone) {
+  console.log("phonebook:");
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(person.name, person.number);
+    });
+    mongoose.connection.close();
+  });
+  return;
+}
+
+const newPerson = new Person({
+  name: userName,
+  number: userPhone,
+});
+
+newPerson.save().then((result) => {
+  console.log(`added ${userName} number ${userPhone} to phonebook`);
+  mongoose.connection.close();
+  return;
+});
