f310cb1b6275eda9ee9b27e5740826b2c582e921
sahaarnav3 <sahaarnav3@gmail.com>
2026-05-23 18:16:45 +0530

    Exercise 7.1 Custom Hooks Done

---
+++ part_7/routed-anecdotes/src/components/CreateNew.jsx
@@ -0,0 +1,38 @@
+import { useNavigate } from 'react-router-dom'
+import { useField } from '../hooks/index'
+
+const CreateNew = ({ addAnecdote }) => {
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    addAnecdote({ content: content.value, author: author.value, info: info.value, votes: 0 })
+    navigate('/')
+  }
+
+  return (
+    <div>
+      <h2>create a new anecdote</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          content
+          <input name='content' value={content.value} onChange={content.onChange} />
+        </div>
+        <div>
+          author
+          <input name='author' value={author.value} onChange={author.onChange} />
+        </div>
+        <div>
+          url for more info
+          <input name='info' value={info.value} onChange={info.onChange} />
+        </div>
+        <button>create</button>
+      </form>
+    </div>
+  )
+}
+
+export default CreateNew
